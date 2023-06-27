420b01c011d0f7452bf0afe96b0f613c4173ae08
SikansiGlobal <joao.lucas.sikansi@globalfinanceiro.com.br>
2023-06-27 19:10:53 -0300

    Início responsividade

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
-
-function ativaLetra(element){
-    const arrTexto = element.innerHTML.split('');
-    element.innerHTML = '';
-    arrTexto.forEach((letra, i) => {
-        setTimeout(() => {
-            element.innerHTML += letra;
-        }, 75 * i);
-    });
+function escrevendoLetra(){
+    function ativaLetra(element){
+        const arrTexto = element.innerHTML.split('');
+        element.innerHTML = '';
+        arrTexto.forEach((letra, i) => {
+            setTimeout(() => {
+                element.innerHTML += letra;
+            }, 75 * i);
+        });
+    }
+    
+    const titulo = document.querySelector('.digitando');
+    
+    ativaLetra(titulo);
 }
 
-const titulo = document.querySelector('.digitando');
+const ativaMenu = document.querySelector('.fa-bars');
 
-ativaLetra(titulo);
+ativaMenu.addEventListener('click', () => {
+    ativaMenu.classList.toggle('fa-x')
+})
+
+escrevendoLetra();
