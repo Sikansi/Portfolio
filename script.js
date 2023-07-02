47879b4b54f3a4a6ce07c452394755960ba72352
SikansiGlobal <joao.lucas.sikansi@globalfinanceiro.com.br>
2023-07-02 19:45:43 -0300

    Seção experiencias estilizada

--- script.js
+++ script.js
@@ -15,9 +15,11 @@ function escrevendoLetra(){
 }
 
 const ativaMenu = document.querySelector('.fa-bars');
+const navMenu = document.querySelector('header .navegacao-primaria');
 
 ativaMenu.addEventListener('click', () => {
-    ativaMenu.classList.toggle('fa-x')
-})
+    ativaMenu.classList.toggle('fa-x');
+    navMenu.classList.toggle('ativado');
+});
 
 escrevendoLetra();
