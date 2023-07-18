c248820c0037fd82458c255eb57c2dc79f64c845
SikansiGlobal <joao.lucas.sikansi@globalfinanceiro.com.br>
2023-07-18 20:12:11 -0300

    Estilização experiencias e educação, skills e fun facts incompleto

--- script.js
+++ script.js
@@ -14,12 +14,66 @@ function escrevendoLetra(){
     ativaLetra(titulo);
 }
 
-const ativaMenu = document.querySelector('.fa-bars');
-const navMenu = document.querySelector('header .navegacao-primaria');
 
-ativaMenu.addEventListener('click', () => {
-    ativaMenu.classList.toggle('fa-x');
-    navMenu.classList.toggle('ativado');
-});
 
 escrevendoLetra();
+
+function menuMobol() {
+    const ativaMenu = document.querySelector('.fa-bars');
+    const navMenu = document.querySelector('header .navegacao-primaria');
+
+    ativaMenu.addEventListener('click', () => {
+        ativaMenu.classList.toggle('fa-x');
+        navMenu.classList.toggle('ativado');
+    });
+}
+
+function sobreMim() {
+
+    const divExperiencia = document.querySelectorAll('.experience-content div');
+    const liExperiencia = document.querySelector('.experience-content ul li');
+    const divEducation = document.querySelectorAll('.education-content div');
+    const liEducation = document.querySelector('.education-content ul li');
+
+    divExperiencia[0].classList.add('ativo');
+    divEducation[0].classList.add('ativo');
+    liExperiencia[0].classList.add('ativo');
+    liEducation[0].classList.add('ativo');
+
+    function slideShow1(index) {
+        divExperiencia.forEach((div) => {
+            div.classList.remove('ativo');
+        });
+        liExperiencia.forEach((botao) => {
+            botao.classList.remove('ativo');
+        });
+        divExperiencia[index].classList.add('ativo');
+        liExperiencia[index].classList.add('ativo');
+    }
+
+    function slideShow2(index) {
+        divEducation.forEach((div) => {
+            div.classList.remove('ativo');
+        });
+        liEducation.forEach((botao) => {
+            botao.classList.remove('ativo');
+        });
+        divEducation[index].classList.add('ativo');
+        liEducation[index].classList.add('ativo');
+    }
+
+    liExperiencia.forEach((event, index) => {
+        event.addEventListener('click', ()=>{
+            slideShow1(index);
+        })
+    })
+
+    liEducation.forEach((event, index) => {
+        event.addEventListener('click', ()=>{
+            slideShow2(index);
+        })
+    })
+
+}
+
+sobreMim();
